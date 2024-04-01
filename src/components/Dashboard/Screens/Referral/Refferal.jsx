c2910ca81357878bd8e488b70fe807fba7ec7292
Major Pinky <107479804+JosephWagio@users.noreply.github.com>
2024-04-01 21:42:29 +0100

    widget

--- src/components/Dashboard/Screens/Referral/Refferal.jsx
+++ src/components/Dashboard/Screens/Referral/Refferal.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Referral = () => {
   return (
-    <div className='main-container'>Refferal</div>
+    <div className='main-container'>
+      <div className="referral-main">
+        <div className="referral-header">
+          <h3>Referral</h3>
+        </div>
+        <div className="referral-body">
+          <div className="referral-invite">
+            <p>Invite your friends through email</p>
+            <input type="text" placeholder='Enter Email Address' />
+            <button className='button'>Invite</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
