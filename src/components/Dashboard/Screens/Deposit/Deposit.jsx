37dd63ff085ced2e453b91e53cd9671142b4a1a9
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-04-24 22:47:35 +0100

    Correction on Api

--- src/components/Dashboard/Screens/Deposit/Deposit.jsx
+++ src/components/Dashboard/Screens/Deposit/Deposit.jsx
@@ -21,7 +21,7 @@ const Deposit = ({ handleCloseSidebar }) => {
     try {
       e.preventDefault()
 
-      const response = await fetch("https://crestbackend.up.railway.app/api/transaction/",
+      const response = await fetch("https://crest-backend.onrender.com/api/transaction/",
         {
           method: "POST",
           headers: {
